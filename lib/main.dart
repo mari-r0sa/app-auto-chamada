7cc19e5dde5b3ba88d1b4508712f32c51a41d889
Marina Rosa Oliveira <marina.rosa@totvs.com.br>
2025-10-09 17:47:39 -0300

    tela de config finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
-import 'package:app_automatizar_chamada/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'screens/home.dart';
+import 'screens/config.dart';
 
 void main() {
-  runApp(const AutoChamadaApp());
+  runApp(const MyApp());
 }
 
-class AutoChamadaApp extends StatelessWidget {
-  const AutoChamadaApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Auto-chamada',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-      home: const HomeScreen(), // tela inicial
+      title: 'Auto-chamada',
+      theme: ThemeData(primaryColor: const Color(0xFF9B1536)),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const HomeScreen(),
+        '/config': (context) => const ConfigScreen(),
+      },
     );
   }
 }
